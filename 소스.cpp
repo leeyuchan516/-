7c002832ce4d7942b696a0267f2951ecce3ea0b7
leeyuchan516 <yuchan20100516@gmail.com>
2025-03-22 18:02:12 +0900

    Add files via upload

--- 소스.cpp
+++ 소스.cpp
@@ -1,145 +1,226 @@
 //#include <stdio.h>
-//int main()
-//{
-//	printf("사과 1개\n");
-//	return 0;
-//}
-//#include <stdio.h>
-//int main()
-//{
-//	int apple = 1;
-//	printf("사과 %d개\n", apple);
-//	return 0;
-//}
-//#include <stdio.h>
-//int main()
-//{
-//	printf("키보드 3개\n");
-//	printf("마우스 2개\n");
-//	return 0;
-//}
-//#include <stdio.h>
-//int main()
-//{
-//	int keyborad = 3;
-//	int mouse = 2;
-//	printf("키보드 : %d개\n", keyborad);
-//		printf("마우스 : %d개\n", mouse);
-//		return 0;
-//}
-//#include <stdio.h>
-//int main()
-//{
-//	int coin1 = 500;
-//	int coin2 = 1000;
-//	printf("%d + %d = %d\n",
-//		coin1, coin2, coin1 + coin2);
-//	return 0;
 //
-//}
-//#include <stdio.h>
-//int main()
+//void main()
 //{
-//	char A;
-//	A = 'A';
-//	printf("A = %c, A = %d\n", A, A);
-//	return 0;
-//}
-//#include <stdio.h>
-//int main()
-//{
-//	double pi;
-//	pi = 3.14;
-//	float e = 2.718;
-//	printf("pi = %.2lf, e = %.3f.. \n", pi, e);
-//	return 0;
-//}
-//#include <stdio.h>
-//int main()
-//{
-//	char A;
-//	A = 'A';
-//	printf("A = %c, A = %d\n", A, A);
-//	return 0;
-//}
-//#include <stdio.h>
-//int main()
-//{
-//	int circle = 5;
-//	double pi;
-//	pi = 3.14;
-//	double area_of_a_circle;
-//	area_of_a_circle = circle * circle * pi;
-//	//반지름 * 반지름 * 파이
-//	printf("원의 넓이 : %.2lf\n", area_of_a_circle);
+//	int a;
+//	float b;
 //
-//	double circumference_of_a_circle;
-//	circumference_of_a_circle = (circle + circle) * pi;
-//	printf("원의 둘래 : %.2lf\n", circumference_of_a_circle);
-//	return 0;
+//	a = 123.45;
+//	b = 200;
+//
+//	printf("a의 값 ==> %d \n", a);
+//	printf("b의 값 ==> %f \n", b);
+//}
+#include <stdio.h>
+
+//void main()
+//{
+//	int a, b;
+//	float c, d;
+//
+//	a = 100;
+//	b = a;
+//
+//	c = 111.1f;
+//	d = c;
+//
+//	printf("a, b의 값 ==> %d , %d \n", a, b);
+//	printf("c, d의 값 ==> %5.1f , %5.1f \n", c, d);
+//}
+//#include <stdio.h>
+//
+//void main()
+//{
+//	int a, b, c, d;
+//
+//	a = 100 + 100;
+//	b = a + 100;
+//	c = a + b - 100;
+//	d = a + b + c;
+//	printf("a, b, c, d의 값 ==> %d, %d, %d, %d \n", a, b, c, d);
+//
+//	a = b = c = d = 100;
+//	printf("a, b, c, d 의 값 ==> %d, %d, %d, %d \n", a, b, c, d);
+//
+//	a = 100;
+//	a = a + 200;
+//	printf("a의 값 ==> %d \n", a);
 //}
 //#include <stdio.h>
 //int main()
 //{
-//	int base = 3;
-//	int top = 5;
-//	int height = 7;
-//	int area_of_trapezoid;
-//	area_of_trapezoid = (base + top) * height / 2;
-//	printf("사다리꼴의 넓이 : %d\n", area_of_trapezoid);
+//	int a = 15;
+//	int b = 3;
+//	printf("%d + %d = %d\n", a, b, a + b);
+//	printf("%d + %d = %d\n", a, b, a - b);
+//	printf("%d * %d = %d\n", a, b, a * b);
+//	printf("%d / %d = %d\n", a, b, a / b);
+//	printf("%d %% %d = %d\n", a, b, a % b);
 //	return 0;
 //}
 //#include <stdio.h>
+//
 //int main()
 //{
-//	int k;
-//	printf("원하는 숫자를 입력 : ");
-//	scanf("%d", &k);
-//	printf("입력한 숫자는 : %d\n", k);
+//	int number = 1;
+//	number = number + 3; printf("%2d\n", number);
+//	number -= 2; printf("%2d\n", number);
+//	number--; printf("%2d\n", number);
 //	return 0;
 //}
 //#include <stdio.h>
+//
 //int main()
 //{
-//	int y, x;
-//	printf("더하고 싶은 값 2개 입력 : ");
-//	scanf("%d %d", &y, &x);
-//	printf("두 값의 합은 %d입니다.\n", y + x);
+//	int a = 0;
+//	printf("%d\n", a++);
+//	printf("%d\n", a);
 //	return 0;
 //}
 //#include <stdio.h>
+//
 //int main()
 //{
-//	char a, b, c;
-//	printf("문자 두 개 입력 : ");
-//	scanf("%c%c%c", &a, &b, &c);
-//	printf("문자출력 : %c%c%c\n", a, b, c);
+//	int a = 0;
+//	printf("%d\n", ++a);
+//	printf("%d\n", a);
 //	return 0;
 //}
 //#include <stdio.h>
+//
 //int main()
 //{
-//	int r;
-//	double pi;
-//	pi = 3.14;
-//	printf("반지름의 길이 : ");
-//	scanf("%d", &r);
-//	double area_of_a_circle;
-//	area_of_a_circle = r * r * pi;
-//	printf("원의 넓이는 %.2lf입니다.\n", area_of_a_circle);
-//	double length_of_circle;
-//	length_of_circle = (r + r) * pi;
-//	printf("원의 둘레는 %.2lf입니다.\n", length_of_circle);
+//	int a, b;
+//	printf("변수 두가지 입력 : ");
+//	scanf("%d%d", &a, &b);
+//	printf("%d + %d = %d\n", a, b, a + b);
+//	printf("%d - %d = %d\n", a, b, a - b);
+//	printf("%d * %d = %d\n", a, b, a * b);
+//	printf("%d / %d = %d\n", a, b, a / b);
+//	printf("%d %% %d = %d\n", a, b, a % b);
+//	return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int first = 1 % 4;
+//	printf("%d = 1%%4\n", first);
+//	int second = 5 % 3;
+//	printf("%d = 5%%3\n", second);
+//	int third = 4 % 2;
+//	printf("%d = 4%%2\n", third);
+//	return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int number1, number2;
+//	scanf("%d %d", &number1, &number2);
+//	printf("%d =1%%4\n", number1);
+//	printf("%d =5%%3\n", number2);
+//	return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int first = 1 % 3;
+//	printf("%d = 1%%3\n", first);
+//	int second = 2 % 3;
+//	printf("%d = 2%%3\n", second);
+//	int third = 3 % 3;
+//	printf("%d = 3%%3\n", third);
+//	int fourth = 4 % 3;
+//	printf("%d = 4%%3\n", fourth);
+//	int fifth = 5 % 3;
+//	printf("%d = 5%%3\n", fifth);
+//	int sixth = 6 % 3;
+//	printf("%d = 6%%3\n", sixth);
+//
+//	return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a;
+//	scanf("%d", &a);
+//	printf("숫자를 입력하시오 :");
+//	printf("%d%%50= %d\n", a, a%50);
+//	return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int data1 = 6 / 2;
+//	printf("%d =6/2\n", data1);
+//	int data2 = 6 / 3;
+//	printf("%d =6/3\n", data2);
+//	int data3 = 6 / 4;
+//	printf("%d =6/4\n", data3);
+//	return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int number1, number2;
+//	scanf("%d %d", &number1, &number2);
+//	printf("%d=", number1 / number2);
+//	printf("%d/%d\n", number1, number2);
+//
+//	return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int coin;
+//	printf("돈을 입력해주세요.(원)");
+//	scanf("%d", &coin);
+//
+//	int coin10000 = coin / 10000;
+//	coin = coin % 10000;
+//	int coin5000 = coin / 5000;
+//	coin = coin % 5000;
+//	int coin1000 = coin / 1000;
+//	printf("만원권 %d개, ", coin10000);
+//	printf("오천원권 %d개, ", coin5000);
+//	printf("천원권 %d개, ", coin1000);
+//	printf("나머지는 동전입니다.");
+//
 //	return 0;
 //}
 #include <stdio.h>
+
 int main()
 {
-	double W1, W2, H;
-	printf("밑변의 길이, 윗변의 길이, 높이 입력 : ");
-	scanf("%lf %lf %lf", &W1, &W2, &H);
-	double area_of_a_trapezoid;
-	area_of_a_trapezoid = (W1 + W2) * H / 2;
-	printf("사다리꼴의 넓이는 %lf입니다.\n", area_of_a_trapezoid);
-	return 0;
+	int jelly;
+	int change;
+	int change = 1000 - jelly;
+	printf("젤리의 가격을 입력해주세요. (원)");
+	scanf("%d", &jelly);
+	printf("젤리를 사고 남은 돈은 %d개입니다", 1000-jelly);
+	
+	int jelly500 = jelly / 500;
+	jelly = jelly % 500;
+	int jelly100 = jelly / 100;
+	jelly = jelly % 100;
+	int jelly50 = jelly / 50;
+	jelly = jelly % 50;
+	int jelly10 = jelly / 10;
+	jelly = jelly % 10;
+	int jelly5 = jelly / 5;
+	jelly = jelly % 5;
+	int jelly1 = jelly / 1;
+	printf("오백원짜리 동전은 %d개,", jelly500);
+	printf("백원짜리 동전은 %d개,", jelly100);
+	printf("오십원짜리 동전은 %d개,", jelly50);
+	printf("십원짜리 동전은 %d개,", jelly10);
+
+
+
 }
