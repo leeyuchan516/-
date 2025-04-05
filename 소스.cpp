4157e93f92e76a3363e2a8a26d8e0ce8d52bf816
leeyuchan516 <yuchan20100516@gmail.com>
2025-04-05 18:02:41 +0900

    Add files via upload

--- 소스.cpp
+++ 소스.cpp
@@ -1,226 +1,271 @@
 //#include <stdio.h>
-//
-//void main()
+//int main()
 //{
-//	int a;
-//	float b;
+//	float a = 123.45;
+//	printf("%08.3f", a);
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	char intial;
+//	scanf("%c", &intial);
+//	//printf("%c", intial);
+//	printf("입력한 문자는");
+//	if (intial >= 0x61 && intial <= 0x7a)
+//	{
+//		printf(" 소문자입니다.");
+//	}
+//	else if (intial >= 65 && intial <='Z')
+//	{
+//		printf(" 대문자입니다.");
+//	}
+//	else
+//	{
+//		printf(" 소문자도 대문자도 아니다.");
+//	}
+//	return 0;
+//}
+//#include <stdio.h>
 //
-//	a = 123.45;
-//	b = 200;
-//
-//	printf("a의 값 ==> %d \n", a);
-//	printf("b의 값 ==> %f \n", b);
+//int main()
+//{
+//	int number, answer;
+//	scanf("%d %d", &number, &answer);
+//	if (number > answer) {
+//		printf("숫자1이 숫자2보다 큽니다.\n");
+//	}
+//	else
+//	{
+//		if (number < answer)
+//		{
+//			printf("숫자2가 숫자1보다 큽니다.\n");
+//		}
+//		else
+//		{
+//			printf("숫자1과 숫자2가 같습니다.\n");
+//		}
+//	}
+//	return 0;
 //}
 #include <stdio.h>
 
-//void main()
-//{
-//	int a, b;
-//	float c, d;
-//
-//	a = 100;
-//	b = a;
-//
-//	c = 111.1f;
-//	d = c;
-//
-//	printf("a, b의 값 ==> %d , %d \n", a, b);
-//	printf("c, d의 값 ==> %5.1f , %5.1f \n", c, d);
-//}
-//#include <stdio.h>
-//
-//void main()
-//{
-//	int a, b, c, d;
-//
-//	a = 100 + 100;
-//	b = a + 100;
-//	c = a + b - 100;
-//	d = a + b + c;
-//	printf("a, b, c, d의 값 ==> %d, %d, %d, %d \n", a, b, c, d);
-//
-//	a = b = c = d = 100;
-//	printf("a, b, c, d 의 값 ==> %d, %d, %d, %d \n", a, b, c, d);
-//
-//	a = 100;
-//	a = a + 200;
-//	printf("a의 값 ==> %d \n", a);
-//}
-//#include <stdio.h>
 //int main()
 //{
-//	int a = 15;
-//	int b = 3;
-//	printf("%d + %d = %d\n", a, b, a + b);
-//	printf("%d + %d = %d\n", a, b, a - b);
-//	printf("%d * %d = %d\n", a, b, a * b);
-//	printf("%d / %d = %d\n", a, b, a / b);
-//	printf("%d %% %d = %d\n", a, b, a % b);
+//	int number, answer;
+// scanf("%d %d", &number, &answer);
+//	if (number > answer) {
+//		printf("숫자1이 숫자2보다 큽니다.\n");
+//	}
+//	else if (number < answer)
+//	{
+//		printf("숫자2가 숫자1보다 큽니다.\n");
+//	}
+//	else
+//	{
+//		printf("숫자1과 2가 같습니다.\n");
+//	}
 //	return 0;
 //}
 //#include <stdio.h>
 //
 //int main()
 //{
-//	int number = 1;
-//	number = number + 3; printf("%2d\n", number);
-//	number -= 2; printf("%2d\n", number);
-//	number--; printf("%2d\n", number);
+//	int score = 75;
+//	if (score > 90) {
+//		printf("성적은 A등급입니다.\n");
+//	}
+//	else if (score > 80) {
+//		printf("성적은 B등급입니다.\n");
+//	}
+//	else if (score > 70) {
+//		printf("성적은 C등급입니다.\n");
+//	}
+//	else if (score > 60) {
+//		printf("성적은 D등급입니다.\n");
+//	}
+//	else {
+//		printf("성적은 F등급입니다.\n");
+//	}
 //	return 0;
 //}
 //#include <stdio.h>
 //
 //int main()
 //{
-//	int a = 0;
-//	printf("%d\n", a++);
-//	printf("%d\n", a);
+//	int number = -11;
+//	if (number > 0) {
+//		printf("number는 양수입니다.\n");
+//	}
+//	else if (number < 0) {
+//		printf("number는 음수입니다.\n");
+//	}
+//	else {
+//		printf("number는 0입니다.\n");
+//	}
+//	if (number % 2 == 0)
+//	{
+//		printf("number는 짝수입니다.\n");
+//	}
+//	else
+//	{
+//		printf("number는 홀수입니다.\n");
+//	}
 //	return 0;
 //}
 //#include <stdio.h>
 //
 //int main()
 //{
-//	int a = 0;
-//	printf("%d\n", ++a);
-//	printf("%d\n", a);
+//	char a;
+//	scanf(" %c", &a);
+//	if (a == 'w') {
+//		printf("'위 방향키를 입력하셨습니다.'");
+//	}
+//	else if (a == 'a') {
+//		printf("'좌 방향키를 입력하셨습니다.'");
+//	}
+//	else if (a == 'd') {
+//		printf("우 방향키를 입력하셨습니다.'");
+//	}
+//	else if (a == 's') {
+//		printf("'아래 방향키를 입력하셨습니다.'");
+//	}
 //	return 0;
 //}
 //#include <stdio.h>
 //
 //int main()
 //{
-//	int a, b;
-//	printf("변수 두가지 입력 : ");
-//	scanf("%d%d", &a, &b);
-//	printf("%d + %d = %d\n", a, b, a + b);
-//	printf("%d - %d = %d\n", a, b, a - b);
-//	printf("%d * %d = %d\n", a, b, a * b);
-//	printf("%d / %d = %d\n", a, b, a / b);
-//	printf("%d %% %d = %d\n", a, b, a % b);
+//	int gauge;
+//	scanf("%d", &gauge);
+//	if (gauge >= 45 && gauge <= 55) {
+//		printf("Perfect");
+//	}
+//	else if (gauge >= 35 && gauge >= 65) {
+//		printf("Excellent");
+//	}
+//	else {
+//		printf("good");
+//	}
 //	return 0;
 //}
 //#include <stdio.h>
 //
 //int main()
 //{
-//	int first = 1 % 4;
-//	printf("%d = 1%%4\n", first);
-//	int second = 5 % 3;
-//	printf("%d = 5%%3\n", second);
-//	int third = 4 % 2;
-//	printf("%d = 4%%2\n", third);
+//	int input;
+//	scanf("%d", &input);
+//	if (input % 3 == 0) {
+//		printf("input은 3의 배수입니다. ");
+//		if (input % 6 == 0) {
+//			printf("input은 6의 배수입니다. ");
+//		}
+//		if (input % 9 == 0) {
+//			printf("input은 9의 배수입니다. ");
+//		}
+//	}
 //	return 0;
 //}
 //#include <stdio.h>
 //
 //int main()
 //{
-//	int number1, number2;
-//	scanf("%d %d", &number1, &number2);
-//	printf("%d =1%%4\n", number1);
-//	printf("%d =5%%3\n", number2);
+//	char command = 'i';
+//
+//	switch (command)
+//	{
+//	case 'i':
+//		printf("아이템창 오픈\n");
+//	}
 //	return 0;
 //}
 //#include <stdio.h>
 //
 //int main()
 //{
-//	int first = 1 % 3;
-//	printf("%d = 1%%3\n", first);
-//	int second = 2 % 3;
-//	printf("%d = 2%%3\n", second);
-//	int third = 3 % 3;
-//	printf("%d = 3%%3\n", third);
-//	int fourth = 4 % 3;
-//	printf("%d = 4%%3\n", fourth);
-//	int fifth = 5 % 3;
-//	printf("%d = 5%%3\n", fifth);
-//	int sixth = 6 % 3;
-//	printf("%d = 6%%3\n", sixth);
-//
+//	char command = 'i';
+//	switch(command)
+//	{
+//	case 'i':
+//		printf("아이템창 오픈\n");
+//	case 'm':
+//		printf("지도창 오픈\n");
+//	}
 //	return 0;
 //}
 //#include <stdio.h>
 //
 //int main()
 //{
-//	int a;
-//	scanf("%d", &a);
-//	printf("숫자를 입력하시오 :");
-//	printf("%d%%50= %d\n", a, a%50);
+//	char command = 'm';
+//
+//	switch (command)
+//	{
+//	case 'i': printf("아이템창 오픈\n");
+//	case 'm': printf("지도창 오픈\n");
+//	}
 //	return 0;
 //}
 //#include <stdio.h>
 //
 //int main()
 //{
-//	int data1 = 6 / 2;
-//	printf("%d =6/2\n", data1);
-//	int data2 = 6 / 3;
-//	printf("%d =6/3\n", data2);
-//	int data3 = 6 / 4;
-//	printf("%d =6/4\n", data3);
+//	char command = 'm';
+//	switch (command)
+//	{
+//	case 'i': printf("아이템창 오픈\n");
+//		break;
+//	case 'm': printf("지도창 오픈\n");
+//	}
 //	return 0;
 //}
 //#include <stdio.h>
 //
 //int main()
 //{
-//	int number1, number2;
-//	scanf("%d %d", &number1, &number2);
-//	printf("%d=", number1 / number2);
-//	printf("%d/%d\n", number1, number2);
-//
+//	char command;
+//	printf("커맨드를 입력하세요:");
+//	scanf("%c", &command);
+//	switch (command)
+//	{
+//	case 'i': printf("아이템창 오픈\n");
+//		break;
+//	case 'm': printf("지도 창 오픈\n");
+//		break;
+//	default: printf("지정된 기능이 없습니다.\n");
+//	}
 //	return 0;
 //}
 //#include <stdio.h>
 //
 //int main()
 //{
-//	int coin;
-//	printf("돈을 입력해주세요.(원)");
-//	scanf("%d", &coin);
-//
-//	int coin10000 = coin / 10000;
-//	coin = coin % 10000;
-//	int coin5000 = coin / 5000;
-//	coin = coin % 5000;
-//	int coin1000 = coin / 1000;
-//	printf("만원권 %d개, ", coin10000);
-//	printf("오천원권 %d개, ", coin5000);
-//	printf("천원권 %d개, ", coin1000);
-//	printf("나머지는 동전입니다.");
-//
+//	char number = 1;
+//	switch (number)
+//	{
+//	case 1: printf("one\n"); break;
+//	case 2: printf("two\n"); break;
+//	case 3:printf("three\n"); break;
+//	}
 //	return 0;
 //}
 #include <stdio.h>
 
 int main()
 {
-	int jelly;
-	int change;
-	int change = 1000 - jelly;
-	printf("젤리의 가격을 입력해주세요. (원)");
-	scanf("%d", &jelly);
-	printf("젤리를 사고 남은 돈은 %d개입니다", 1000-jelly);
-	
-	int jelly500 = jelly / 500;
-	jelly = jelly % 500;
-	int jelly100 = jelly / 100;
-	jelly = jelly % 100;
-	int jelly50 = jelly / 50;
-	jelly = jelly % 50;
-	int jelly10 = jelly / 10;
-	jelly = jelly % 10;
-	int jelly5 = jelly / 5;
-	jelly = jelly % 5;
-	int jelly1 = jelly / 1;
-	printf("오백원짜리 동전은 %d개,", jelly500);
-	printf("백원짜리 동전은 %d개,", jelly100);
-	printf("오십원짜리 동전은 %d개,", jelly50);
-	printf("십원짜리 동전은 %d개,", jelly10);
-
-
-
+	int input1, input2;
+	char oper;
+	printf("첫 번쩨 계산할 값 ==>");
+	scanf("%d", &input1);
+	printf("두 번째 계산할 값 ==>");
+	scanf("%d", &input2);
+	printf("+ - * % ==>");
+	scanf("%c", &oper);
+	switch (oper)
+	{
+	case +: printf("%d", input1 + input2); break;
+	case -: printf("%d", input1 - input2); break;
+	case* : printf("%d", input1 * input2); break;
+	}
 }
