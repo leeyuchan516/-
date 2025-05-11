6b9e68e00921a194a5e698765bff166f66a4fac4
leeyuchan516 <yuchan20100516@gmail.com>
2025-05-11 15:59:31 +0900

    Add files via upload

--- 소스.cpp
+++ 소스.cpp
@@ -1,571 +1,110 @@
 //#include <stdio.h>
-//int main()
+//void main()
 //{
-//	int outer;
-//	int inter;
-//	for (outer = 0; outer < 5; outer++) {
-//		for (inter = 0; inter < 5; inter++) {
-//			printf(" * ");
+//	int i, k;
 //
-//		}
-//		printf("\n");
-//	}
-//	return 0;
+//	for (i = 1, k = 2; i <= 9; i++, k++)
+//		printf("%d * %d = %d\n", i, k, i * k);
 //}
 //#include <stdio.h>
-//int main()
+//void main()
 //{
-//	int outer;
-//	int inter;
-//	for (outer = 0; outer < 5; outer++) {
-//		for (inter = 0; inter < outer + 1; inter++) {
-//			printf("*");
-//		}
-//		printf("\n");
+//	int i;
+//	i = 0;
+//	for (;;)
+//	{
+//		printf("%d \n", i);
+//		i++;
 //	}
-//	return 0;
 //}
+//#define _CHR_SURE_NO_WARNINGS
 //#include <stdio.h>
+//void main()
+//{
+//	int a, b;
 //
-//int main()
-//{
-//	int outer, inter1, inter2;
-//	for (outer = 0; outer < 5; outer++) {
-//		for (inter1 = 0; inter1 < 5 - outer; inter1++) {
-//			printf(" ");
-//		}
-//		for (inter2 = 0; inter2 < outer + 1; inter2++) {
-//			printf("*");
-//		}
-//		printf("\n");
-//	}
-//	return 0;
-//}
-//#include <stdio.h>
-//int main()
-//{
-//	int outer;
-//	int inter1, inter2;
-//	for (outer = 0; outer < 5 ; outer++) {
-//		for (inter1 = 0; inter1 < 5 - outer; inter1++) {
-//			printf(" ");
-//		}
-//		for (inter2 = 0; inter2 < outer + 1; inter2++) {
-//			printf("*");
-//		}
-//		printf("\n");
-//	}
-//	return 0;
-//}
-//#include <stdio.h>
-//int main()
-//{
-//	int outer;
-//	int inter1;
+//	for (;;)
+//	{
+//		printf("더할 두 수 입력 (멈추려면 Ctrl+C) :");
+//		scanf("%d %d", &a, &b);
 //
-//	for (outer = 0; outer < 4; outer++) {
-//		for (inter1 = 0; inter1 < 4 - outer; inter1++) {
-//			printf("*");
-//		}
-//		printf("\n");
+//		printf("%d + %d = %d \n", a, b, a + b);
 //	}
 //}
 //#include <stdio.h>
-//int main()
+//void main()
 //{
-	/*printf("*");
-	printf("\n");
-	printf("*");
-	printf("*");
-	printf("\n");
-	printf("*");
-	printf("*");
-	printf("*");
-	printf("\n");*/
-
-	/*for (int x = 1; x <= 1; x++) {
-		printf("*");
-	}
-	printf("\n");
-
-	for (int x = 1; x <= 2; x++) {
-		printf("*");
-	}
-	printf("\n");
-
-	for (int x = 1; x <= 3; x++) {
-		printf("*");
-	}
-	printf("\n");*/
-
-
-	/*int y = 1;
-
-	for (int x = 1; x <= y; x++) {
-		printf("*");
-	}
-	printf("\n");
-	y++;
-
-	for (int x = 1; x <= y; x++) {
-		printf("*");
-	}
-	printf("\n");
-	y++;
-
-	for (int x = 1; x <= y; x++) {
-		printf("*");
-	}
-	printf("\n");
-	y++;*/
-
-	/*int y = 1;
-
-	for (;y<=3;) {
-		for (int x = 1; x <= y; x++) {
-			printf("*");
-		}
-		printf("\n");
-		y++;
-	}*/
-
-	
-	/*for (int y = 1; y <= 3; y++) {
-		for (int x = 1; x <= y; x++) {
-			printf("*");
-		}
-		printf("\n");
-	}*/
+//	int i;
 //
-//	int LINE = 3;
-//	printf("몇줄?>>");
-//	scanf("%d", &LINE);
+//	for (i = 0; i < 128; i++)
+//	{
+//		if (i % 16 == 0)
+//		{
+//			printf("------------------\n");
+//			printf("10진수 16진수 문자\n");
+//			printf("------------------\n");
+//		}
+//		printf("%5d %5x %5c\n", i, i, i);
+//	}
+//}
+//#include <stdio.h>
+//void main()
+//{
+//	int a;
+//	int b;
 //
-//	for (int y = 1; y <= LINE; y++) {
-//		for (int x = 1; x <= y; x++) {
-//			printf("*");
-//		}
-//		printf("\n");
-//	}
-//	return 0;
-//}
-//#include <stdio.h>
-//int main()
-//{
-	//printf("*");
-	//printf("*");
-	//printf("*");
-	//printf("*");
-	//printf("\n");
-	//printf("*");
-	//printf("*");
-	//printf("*");
-	//printf("\n");
-	//printf("*");
-	//printf("*");
-	//printf("\n");
-	//printf("*");
-	//printf("\n");
-
-	//for (int w = 1; w <= 4; w++) {
-	//	printf("*");
-	//}
-	//printf("\n");
-	//for (int w = 1; w <= 3; w++) {
-	//	printf("*");
-	//}
-	//printf("\n");
-	//for (int w = 1; w <= 2; w++) {
-	//	printf("*");
-	//}
-	//printf("\n");
-	//for (int w = 1; w <= 1; w++) {
-	//	printf("*");
-	//}
-	//printf("\n");
-
-	//int y = 4;
-	//for (int w = 1; w <= y; w++) {
-	//	printf("*");
-	//}
-	//printf("\n");
-	//y--;
-	//for (int w = 1; w <= y; w++) {
-	//	printf("*");
-	//}
-	//printf("\n");
-	//y--;
-	//for (int w = 1; w <= y; w++) {
-	//	printf("*");
-	//}
-	//printf("\n");
-	//y--;
-	//for (int w = 1; w <= y; w++) {
-	//	printf("*");
-	//}
-	//printf("\n");
-	//y--;
-
-	//int y = 4;
-	//for (; y >= 1;) {
-	//	for (int w = 1; w <= y; w++) {
-	//		printf("*");
-	//	}
-	//	printf("\n");
-	//	y--;
-	//}
-
-	//for (int y = 4; y >= 1; y--) {
-	//	for (int w = 1; w <= y; w++) {
-	//		printf("*");
-	//	}
-	//	printf("\n");
-	//}
-
-//	int LINE = 3;
-//	printf("몇줄? ");
-//	scanf("%d", &LINE);
-//	for (int y = LINE ; y >= 1; y--) {
-//		for (int w = 1; w <= y; w++) {
-//			printf("*");
+//	for (a = 9; a >= 1; a--) 
+//	{
+//		for (b = 9; b >= 2; b--)
+//		{
+//			printf("%d * %d = %2d   ", b, a, a * b);
 //		}
 //		printf("\n");
 //	}
 //}
 //#include <stdio.h>
-//int main()
+//void main()
 //{
-	//printf(" ");
-	//printf(" ");
-	//printf(" ");
-	//printf("*");
-	//printf("\n");
-	//printf(" ");
-	//printf(" ");
-	//printf("*");
-	//printf("*");
-	//printf("*");
-	//printf("\n");
-	//printf(" ");
-	//printf("*");
-	//printf("*");
-	//printf("*");
-	//printf("*");
-	//printf("*");
-	//printf("\n");
-	//printf("*");
-	//printf("*");
-	//printf("*");
-	//printf("*");
-	//printf("*");
-	//printf("*");
-	//printf("*");
-	//printf("\n");
-
-	//for (int a = 1; a <= 3; a++) {
-	//	printf(" ");
-	//}
-	//for (int a = 1; a <= 1; a++) {
-	//	printf("*");
-	//}
-	//printf("\n");
-	//for (int a = 1; a <= 2; a++) {
-	//	printf(" ");
-	//}
-	//for (int a = 1; a <= 3; a++) {
-	//	printf("*");
-	//}
-	//printf("\n");
-	//for (int a = 1; a <= 1; a++) {
-	//	printf(" ");
-	//}
-	//for (int a = 1; a <= 5; a++) {
-	//	printf("*");
-	//}
-	//printf("\n");
-	//for (int a = 1; a <= 7; a++) {
-	//	printf("*");
-	//}
-	//printf("\n");
-
-//	int b = 3;
-//	int c = 1;
-//	for (int a = 1; a <= b; a++) {
-//		printf(" ");
+//	int i;
+//	int a = 0;
+//	int b = 0;
+//
+//	for (i = 1; i <= 100; i++)
+//	{
+//		if (i % 2 == 0)
+//			a = a + i;
+//		else
+//			b = b + i;
 //	}
-//	b--;
-//	for (int a = 1; a <= c; a++) {
-//		printf("*");
-//	}
-//	c += 2;
-//	printf("\n");
-//	for (int a = 1; a <= b; a++) {
-//		printf(" ");
-//	}
-//	b--;
-//	for (int a = 1; a <= c; a++) {
-//		printf("*");
-//	}
-//	c += 2;
-//	printf("\n");
-//	for (int a = 1; a <= b; a++) {
-//		printf(" ");
-//	}
-//	b--;
-//	for (int a = 1; a <= c; a++) {
-//		printf("*");
-//	}
-//	c += 2;
-//	printf("\n");
-//	for (int a = 1; a <= b; a++) {
-//		printf(" ");
-//	}
-//	b--;
-//	for (int a = 1; a <= c; a++) {
-//		printf("*");
-//	}
-//	c += 2;
-//	printf("\n");
-
-	//int b = 3;
-	//int c = 1;
-	//for (; b >= 0;) {
-	//	for (int a = 1; a <= b; a++) {
-	//		printf(" ");
-	//	}
-	//	b--;
-	//	for (int a = 1; a <= c; a++) {
-	//		printf("*");
-	//	}
-	//	c += 2;
-	//	printf("\n");
-	//}
-
-	//int c = 1;
-	//for (int b = 3; b >= 0; b--) {
-	//	for (int a = 1; a <= b; a++) {
-	//		printf(" ");
-	//	}
-	//	for (int a = 1; a <= c; a++) {
-	//		printf("*");
-	//	}
-	//	c += 2;
-	//	printf("\n");
-	//}
-
-//	int LINE = 3;
-//	int c = 1;
-//	printf("몇줄? ");
-//	scanf("%d", &LINE);
-//	for (int b = LINE - 1; b >= 0; b--) {
-//		for (int a = 1; a <= b; a++) {
-//			printf(" ");
-//		}
-//		for (int a = 1; a <= c; a++) {
-//			printf("*");
-//		}
-//		c += 2;
-//		printf("\n");
-//	}
+//
+//	printf("%d", a);
+//	printf("%d", b);
 //}
 #include <stdio.h>
+#include <string.h>
 int main()
 {
-	//printf(" ");
-	//printf(" ");
-	//printf(" ");
-	//printf("*");
-	//printf("\n");
-	//printf(" ");
-	//printf(" ");
-	//printf("*");
-	//printf("*");
-	//printf("*");
-	//printf("\n");
-	//printf(" ");
-	//printf("*");
-	//printf("*");
-	//printf("*");
-	//printf("*");
-	//printf("*");
-	//printf("\n");
-	//printf("*");
-	//printf("*");
-	//printf("*");
-	//printf("*");
-	//printf("*");
-	//printf("*");
-	//printf("*");
-	//printf("\n");
-	//printf("*");
-	//printf("*");
-	//printf("*");
-	//printf("*");
-	//printf("*");
-	//printf("*");
-	//printf("*");
-	//printf("\n");
-	//printf(" ");
-	//printf("*");
-	//printf("*");
-	//printf("*");
-	//printf("*");
-	//printf("*");
-	//printf("\n");
-	//printf(" ");
-	//printf(" ");
-	//printf("*");
-	//printf("*");
-	//printf("*");
-	//printf("\n");
-	//printf(" ");
-	//printf(" ");
-	//printf(" ");
-	//printf("*");
-	//printf("\n");
+	char str[100];
+	int str_cnt;
+	int i;
 
-	//for (int a = 1; a <= 3; a++) {
-	//	printf(" ");
-	//}
-	//for (int a = 1; a <= 1; a++) {
-	//	printf("*");
-	//}
-	//printf("\n");
-	//for (int a = 1; a <= 2; a++) {
-	//	printf(" ");
-	//}
-	//for (int a = 1; a <= 3; a++) {
-	//	printf("*");
-	//}
-	//printf("\n");
-	//for (int a = 1; a <= 1; a++) {
-	//	printf(" ");
-	//}
-	//for (int a = 1; a <= 5; a++) {
-	//	printf("*");
-	//}
-	//printf("\n");
-	//for (int a = 1; a <= 7; a++) {
-	//	printf("*");
-	//}
-	//printf("\n");
-	//for (int a = 1; a <= 7; a++) {
-	//	printf("*");
-	//}
-	//printf("\n");
-	//for (int a = 1; a <= 1; a++) {
-	//	printf(" ");
-	//}
-	//for (int a = 1; a <= 5; a++) {
-	//	printf("*");
-	//}
-	//printf("\n");
-	//for (int a = 1; a <= 2; a++) {
-	//	printf(" ");
-	//}
-	//for (int a = 1; a <= 3; a++) {
-	//	printf("*");
-	//}
-	//printf("\n");
-	//for (int a = 1; a <= 3; a++) {
-	//	printf(" ");
-	//}
-	//for (int a = 1; a <= 1; a++) {
-	//	printf("*");
-	//}
+	printf("영문자 및 숫자를 입력 (100자 이하) : ");
+	scanf("%s", str);
 
-	//int b = 3;
-	//int c = 1;
-	//for (int a = 1; a <= b; a++) {
-	//	printf(" ");
-	//}
-	//b--;
-	//for (int a = 1; a <= c; a++) {
-	//	printf("*");
-	//}
-	//c += 2;
-	//printf("\n");
-	//for (int a = 1; a <= b; a++) {
-	//	printf(" ");
-	//}
-	//b--;
-	//for (int a = 1; a <= c; a++) {
-	//	printf("*");
-	//}
-	//c += 2;
-	//printf("\n");
-	//for (int a = 1; a <= b; a++) {
-	//	printf(" ");
-	//}
-	//b--;
-	//for (int a = 1; a <= c; a++) {
-	//	printf("*");
-	//}
-	//c += 2;
-	//printf("\n");
-	//for (int a = 1; a <= b; a++) {
-	//	printf(" ");
-	//}
-	//for (int a = 1; a <= c; a++) {
-	//	printf("*");
-	//}
-	//printf("\n");
-	//for (int a = 1; a <= b; a++) {
-	//	printf(" ");
-	//}
-	//b++;
-	//for (int a = 1; a <= c; a++) {
-	//	printf("*");
-	//}
-	//c -= 2;
-	//printf("\n");
-	//for (int a = 1; a <= b; a++) {
-	//	printf(" ");
-	//}
-	//b++;
-	//for (int a = 1; a <= c; a++) {
-	//	printf("*");
-	//}
-	//c -= 2;
-	//printf("\n");
-	//for (int a = 1; a <= b; a++) {
-	//	printf(" ");
-	//}
-	//b++;
-	//for (int a = 1; a <= c; a++) {
-	//	printf("*");
-	//}
-	//c -= 2;
-	//printf("\n");
-	//for (int a = 1; a <= b; a++) {
-	//	printf(" ");
-	//}
-	//b++;
-	//for (int a = 1; a <= c; a++) {
-	//	printf("*");
-	//}
+	printf("\n");
+	printf("입력한 문자열 ==> %s\n", str);
+	printf("변한된 문자열 ==>");
 
-	int b = 3;
-	int c = 1;
-	for (; b >= 0;) {
-		for (int a = 1; a <= b; a++) {
-			printf(" ");
+	str_cnt = strlen(str);
+	/*printf("%c", str[0]);*/
+
+	for (str_cnt; str_cnt >= 0; str_cnt--)
+	{
+		if (str[str_cnt]>='0' && str[str_cnt]<='9')
+		{
+			printf("#");
 		}
-		b--;
-		for (int a = 1; a <= c; a++) {
-			printf("*");
-		}
-		c += 2;
-		printf("\n");
+		else
+		printf("%c", str[str_cnt]);
 	}
-	b++;
-	c -= 2;
-	for (int a = 1; a <= b; a++) {
-	printf(" ");
-	}
-	b++;
-	for (int a = 1; a <= c; a++) {
-		printf("*");
-	}
-	c -= 2;
 }
