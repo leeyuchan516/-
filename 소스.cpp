ff64913e1c3b4df145b56bbb6c85328cdbba72c8
leeyuchan516 <yuchan20100516@gmail.com>
2025-06-14 17:59:59 +0900

    Add files via upload

--- 소스.cpp
+++ 소스.cpp
@@ -1,389 +1,69 @@
 //#include <stdio.h>
+//
 //int main()
 //{
-//	printf(" ");
-//	printf(" ");
-//	printf(" ");
-//	printf("1");
-	//printf("\n");
-	//printf(" ");
-	//printf(" ");
-	//printf("2");
-	//printf("2");
-	//printf("\n");
-	//printf(" ");
-	//printf("3");
-	//printf("3");
-	//printf("3");
-	//printf("\n");
-	//printf("4");
-	//printf("4");
-	//printf("4");
-	//printf("4");
-
-	//for (int x = 1; x <= 3; x++)
-	//{
-	//	printf(" ");
-	//}
-	//
-	//for (int x = 1; x <= 1; x++)
-	//{
-	//	printf("1");
-	//}
-	//printf("\n");
-	//for (int x = 1; x <= 2; x++)
-	//{
-	//	printf(" ");
-	//}
-	//for (int x = 1; x <= 2; x++)
-	//{
-	//	printf("2");
-	//}
-	//printf("\n");
-	//for (int x = 1; x <= 1; x++)
-	//{
-	//	printf(" ");
-	//}
-	//for (int x = 1; x <= 3; x++)
-	//{
-	//	printf("3");
-	//}
-	//printf("\n");
-	//for (int x = 1; x <= 0; x++)
-	//{
-	//	printf(" ");
-	//}
-	//for (int x = 1; x <= 4; x++)
-	//{
-	//	printf("4");
-	//}
-	//int y = 3;
-	//int a = 1;
-	//for (int x = 1; x <= y; x++)
-	//{
-	//	printf(" ");
-	//}
-	//y -= 1;
-	//for(int x = 1; x <= a; x++)
-	//printf("1");
-	//a += 1;
-	//printf("\n");
-	//for (int x = 1; x <= y; x++)
-	//{
-	//	printf(" ");
-	//}
-	//y -= 1;
-	//for (int x = 1; x <= a; x++)
-	//	printf("2");
-	//a += 1;
-	//printf("\n");
-	//for (int x = 1; x <= y; x++)
-	//{
-	//	printf(" ");
-	//}
-	//y -= 1;
-	//for (int x = 1; x <= a; x++)
-	//	printf("3");
-	//a += 1;
-	//printf("\n");
-	//for (int x = 1; x <= y; x++)
-	//{
-	//	printf(" ");
-	//}
-	//y -= 1;
-	//for (int x = 1; x <= a; x++)
-	//	printf("4");
-	//a += 1;
-
-	//int y = 3;
-	//int a = 1;
-
-	//for (; y >= 0;) 
-	//{
-	//	for (int x = 1; x <= y; x++)
-	//	{
-	//		printf(" ");
-	//	}
-	//	for (int x = 1; x <= a; x++)
-	//	{
-	//		printf("*");
-	//	}
-	//	printf("\n");
-	//	y -= 1;
-	//	a += 1;
-	//}
-	// 
-	//int a = 1;
-
-	//for (int y = 3; y >= 0; y -= 1)
-	//{
-	//	for (int x = 1; x <= y; x++)
-	//	{
-	//		printf(" ");
-	//	}
-	//	for (int x = 1; x <= a; x++)
-	//	{
-	//		printf("%d", a);
-	//	}
-	//	a += 1;
-	//	printf("\n");
-	//}
-	//int LINE = 4;
-	//int y = 3;
-
-	//for (int a = 1; a <= LINE; a += 1)
-	//{
-	//	for (int x = 1; x <= y; x++)
-	//	{
-	//		printf(" ");
-	//	}
-	//	for (int x = 1; x <= a; x++)
-	//	{
-	//		printf("%d", a);
-	//	}
-	//	y -= 1;
-	//	printf("\n");
-	//}
-//	int LINE = 4;
-//	scanf("%d", &LINE);
-//	int y = LINE - 1;
+//	char p;
+//	int a, b, c;
+//	
+//	printf("연산 선택 (+ 또는 *) ==> ");
+//	scanf("%c", &p);
+//	printf("첫 번째 값 ==> ");
+//	scanf("%d", &a);
+//	printf("두 번째 값 ==> ");
+//	scanf("%d", &b);
+//	printf("세 번째 값 ==> ");
+//	scanf("%d", &c);
 //
-//	for (int a = 1; a <= LINE; a += 1)
+//	if (p == '+')
 //	{
-//		for (int x = 1; x <= y; x++)
-//		{
-//			printf(" ");
-//		}
-//		for (int x = 1; x <= a; x++)
-//		{
-//			printf("%d", a);
-//		}
-//		y -= 1;
-//		printf("\n");
+//		printf("%d + %d + %d = %d\n", a, b, c, a + b + c);
+//	}
+//	else if (p == '*')
+//	{
+//		printf("%d * %d * %d = %d", a, b, c, a * b * c);
 //	}
 //}
 //#include <stdio.h>
+//
 //int main()
 //{
-//
-//	int input = 0;
-//
-//	printf("<베스킨 라빈스 31!>\n");
-//	printf("게임을 시작합니다! : 뒤에 숫자를 입력해주세요.\n");
-//
-//	int player = 0;
-//	for (;;)
+//	int a;
+//	int b;
+//	printf("## 두 수를 나누기 ##\n");
+//	printf("첫 번째 값 ==> ");
+//	scanf("%d", &a);
+//	printf("두 번째 값 ==> ");
+//	scanf("%d", &b);
+//	if (b == 0)
 //	{
-//		for (int x = 1; x <= 3; x++)
+//		printf("다시 입력해주세요. 두 번째 값 ==> ");
+//		scanf("%d", &b); 
+//		if (b == 0)
 //		{
-//			printf("%c : ", 'A' + player);
-//			scanf("%d", &input);
-//		}
-//		printf("턴이 넘어갑니다.\n");
-//
-//		player = !player;
-//	}
-//}
-
-
-//#include <stdio.h>
-//int main()
-//{
-//
-//	int input = 0;
-//
-//	printf("<베스킨 라빈스 31!>\n");
-//	printf("게임을 시작합니다! : 31을 말하는 사람이 지게됩니다.\n");
-//
-//	int player = 0;
-//	for (;;)
-//	{
-//		for (int x = 1; x <= 3; x++)
-//		{
-//			printf("%c : ", 'A' + player);
-//			scanf("%d", &input);
-//			if (input >= 31) {
-//				break;
+//			printf("또 다시 입력해주세요. 두 번째 값 ==> ");
+//			scanf("%d", &b);
+//			if (b == 0) {
+//				printf("0을 3번 입력했습니다. 종료합니다.\n");
+//				return 0;
 //			}
 //		}
-//		player = !player;
-//		if (input >= 31) {
-//			break;
-//		}
-//		printf("턴이 넘어갑니다.\n");
 //	}
-//	printf("유저 %c의 승리입니다!", 'A' + player);
+//	printf("%d / %d = %d", a, b, a / b);
 //}
-//#include <stdio.h>
-//int main()
-//{
-//
-//	int input = 0;
-//
-//	printf("<베스킨 라빈스 31!>\n");
-//	printf("게임을 시작합니다! : 31을 말하는 사람이 지게됩니다.\n");
-//
-//	int player = 0;
-//	int input2 = 1;
-//	for (;;)
-//	{
-//		for (int x = 1; x <= 3; x++)
-//		{
-//			printf("지금 입력해야하는 값은 %d입니다.\n", input2);
-//			input2++;
-//			printf("%c : ", 'A' + player);
-//			scanf("%d", &input);
-//			if (input >= 31) {
-//				break;
-//			}
-//		}
-//		player = !player;
-//		if (input >= 31) {
-//			break;
-//		}
-//		printf("턴이 넘어갑니다.\n");
-//	}
-//	printf("유저 %c의 승리입니다!", 'A' + player);
-//}
-//#include <stdio.h>
-//int main()
-//{
-//
-//	int input = 0;
-//
-//	printf("<베스킨 라빈스 31!>\n");
-//	printf("게임을 시작합니다! : 31을 말하는 사람이 지게됩니다.\n");
-//
-//	int player = 0;
-//	int input2 = 1;
-//	for (;;)
-//	{
-//		for (int x = 1; x <= 3; x++)
-//		{
-//			printf("%c : ", 'A' + player);
-//			scanf("%d", &input);
-//			if (input != input2)
-//			{
-//				printf("잘못된 입력입니다\n");
-//				printf("지금 입력해야하는 값은 %d입니다.\n", input2);
-//				x--;
-//				continue;
-//				
-//			}
-//			input2++;
-//			if (input >= 31) {
-//				break;
-//			}
-//		}
-//		player = !player;
-//		if (input >= 31) {
-//			break;
-//		}
-//		printf("턴이 넘어갑니다.\n");
-//	}
-//	printf("유저 %c의 승리입니다!", 'A' + player);
-//}
-//#include <stdio.h>
-//int main()
-//{
-//
-//	int input = 0;
-//
-//	printf("<베스킨 라빈스 31!>\n");
-//	printf("게임을 시작합니다! : 31을 말하는 사람이 지게됩니다. (0 입력시 턴 넘김)\n");
-//
-//	int player = 0;
-//	int input2 = 1;
-//	for (;;)
-//	{
-//		for (int x = 1; x <= 3; x++)
-//		{
-//			printf("%c : ", 'A' + player);
-//			scanf("%d", &input);
-//			if (input == 0)
-//			{
-//				if (x == 1)
-//				{
-//					printf("턴을 넘기기 위해서는 하나 이상의 수를 말해야 합니다.\n");
-//					x--;
-//					continue;
-//				}
-//				else
-//				{
-//					break;
-//				}
-//			}
-//			if (input != input2)
-//			{
-//				printf("잘못된 입력입니다\n");
-//				printf("지금 입력해야하는 값은 %d입니다.\n", input2);
-//				x--;
-//				continue;
-//
-//			}
-//			input2++;
-//			if (input >= 31) {
-//				break;
-//			}
-//		}
-//		player = !player;
-//		if (input >= 31) {
-//			break;
-//		}
-//		printf("턴이 넘어갑니다.\n");
-//	}
-//	printf("유저 %c의 승리입니다!", 'A' + player);
-//}
-//#include <stdio.h>
-//
-//int main()
-//{
-//	float conversion = 0;
-//	while (conversion < 2)
-//	{
-//		printf("%.1f\n", conversion);
-//		conversion = conversion + 0.1;
-//	}
-//	return 0;
-//}
-//#include <stdio.h>
-//int main()
-//{
-//	for (double conversion = 90; conversion > 0.1; conversion /= 3)
-//	{
-//		if (conversion == 10.0) {
-//			continue;
-//		}
-//		printf("%.31f\n", conversion);
-//	}
-//	return 0;
-//}
-//#include <stdio.h>
-//
-//int main()
-//{
-//	int number;
-//	while (1) {
-//		scanf("%d", &number);
-//		if (number == 0) {
-//			printf("프로그램 종료 \n");
-//			break;
-//		}
-//	}
-//	return 0;
-//}
-// 
 //#include <stdio.h>
 //
 //int main()
 //{
 //	int for_flag = 1;
 //	int number = 0;
-//	while (for_flag) {
+//	while (for_flag)
+//	{
 //		printf("number = %d\n", number);
-//		while (number++) {
+//		while (number++)
+//		{
 //			printf(" >number = %d\n", number);
-//			if (number == 3) {
+//			if (number == 3)
+//			{
 //				for_flag = 0;
 //				break;
 //			}
@@ -394,3 +74,58 @@
 //	printf("%d %d\n", for_flag, number);
 //	return 0;
 //}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	float conversion = 0;
+//	for (; conversion < 2; conversion += 0.1) {
+//		printf("%.1f\n", conversion + 0.1);
+//	}
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	double conversion = 90;
+//	while (conversion > 0.1)
+//	{
+//		
+//		if (conversion == 10.0) {
+//			conversion /= 3;
+//			continue;
+//		}
+//		printf("%.31f\n", conversion);
+//			conversion /= 3;
+//	}
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	for (double conversion = 90; //1
+//		conversion > 0.1; //2 6 10 13
+//		conversion /= 3)//5 9 12
+//	{
+//		if (conversion == 10.0) {//3 7 11
+//			continue;
+//		}
+//		printf("%.31f\n", conversion);//4 8
+//	}
+//	return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int number;
+//	for (;;)
+//	{
+//		scanf("%d", &number);
+//		if (number == 0) {
+//			printf("프로그램 종료\n");
+//			break;
+//		}
+//	}
+//	return 0;
+//}
+ 
