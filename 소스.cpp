a77e2329b9f617fb6c749870e6178299571a3c8c
leeyuchan516 <yuchan20100516@gmail.com>
2025-07-12 18:01:18 +0900

    Add files via upload

--- 소스.cpp
+++ 소스.cpp
@@ -2,101 +2,210 @@
 //
 //int main()
 //{
-//	int a;//현재 남아 있는 돈//
-//	int b;//몇 장//
+//	int a[10] = { 1, 1, 4, 5, 6, 2, 3, 3, 4, 1 };	
+//	int dice[7] = { };
 //
-//	printf("## 지폐로 교환할 돈은 ? ");
-//	scanf("%d", &a);
-//	printf("\n\n");
-//
-//	b = a / 50000;
-//	printf("오만 원짜리 ==> %d 장 \n", b);
-//	a = a % 50000;
-//
-//	b = a / 10000;
-//	printf("만 원짜리 ==> %d 장 \n", b);
-//	a = a % 10000;
-//
-//	b = a / 5000;
-//	printf("오천 원짜리 ==> %d 장 \n", b);
-//	a = a % 5000;
-//	
-//	b = a / 1000;
-//	printf("천 원짜리 ==> %d 장 \n", b);
-//	a = a % 1000;
-//
-//	printf("지폐로 바꾸지 못한 잔돈 ==> %d 원", a);
-//}
-//
-//#define _CHR_SURE_NO_WARINGS
-//#include <stdio.h>
-//void main()
-//{
-//	int a, b, c, d;
-//	int hap;
-//
-//	printf("1번째 숫자를 입력하시오: ");
-//	scanf("%d", &a);
-//	printf("2번째 숫자를 입력하시요: ");
-//	scanf("%d", &b);
-//	printf("3번째 숫자를 입력하시오: ");
-//	scanf("%d", &c);
-//	printf("4번째 숫자를 입력하시오: ");
-//	scanf("%d", &d);
-//	hap = a + b + c + d;
-//	printf(" 합계 ==> %d \n", hap);
-//}
-//
-//#define _CHR_SECURE_NO_WARINGS
-//#include <stdio.h>
-//void main()
-//{
-//	int aa[4];
-//	int hap;
-//
-//	printf("1번째 숫자를 입력하시오 : ");
-//	scanf("%d", &aa[0]);
-//	printf("2번째 숫자를 입력하시오 : ");
-//	scanf("%d", &aa[1]);
-//	printf("3번째 숫자를 입력하시오 : ");
-//	scanf("%d", &aa[2]);
-//	scanf("4번째 숫자를 입력하시오 : ");
-//	scanf("%d", &aa[3]);
-//
-//	hap = aa[0] + aa[1] + aa[2] + aa[3];
-//	
-//	printf(" 합게 ==> %d \n", hap);
-//}
-//
-//#define _CHR_SECURE_WARINGS
-//#include <stdio.h>
-//void main()
-//{
-//	int aa[4];
-//	int hap = 0;
-//	int i;
-//
-//	for (i = 0; i <= 3; i++)
+//	for (int k = 0; k < 10; k++)//10
 //	{
-//		printf("%d번째 숫자를 입력하시오 : ", i + 1);
-//		scanf("%d", &aa[i]);
+//		dice[a[k]]++;
+//	}
+//	for (int k = 1; k <= 6; k++)
+//	{
+//		printf("주사위 %d가 나온 횟수는 %d\n", k, dice[k]);
+//	}
+//}
+//66
+//16
+//#include <stdio.h>
+//
+//int main()
+//{
+//	char arr[2][3] = { '1', '2', '3', '4', '5', '6' };
+//	printf("%d\n", sizeof(arr));
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int arr[2][3] = { {'1', '2', '3'},{'4', '5', '6'} };
+//	printf("%d\n", sizeof(arr));
+//	return 0;
+//}
+
+//#include <stdio.h>
+//void main()
+//{
+//	int arr2d[5][4] =
+//	{ {662, 7, 4, 74}, {8, 396, 299, 95}, {66, 73, 86, 0}, {84, 7, 41, 11}, };
+//	for (int i = 0; i < 5; i++)
+//	{
+//		for (int j = 0; j < 4; j++)
+//		{
+//			printf("%3d", arr2d[i][j]);
+//		}
+//		printf("\n");
+//	}
+//}
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a[3][3] = { {2, 4, 6}, {8, 10, 12}, {14, 16, 18} };
+//
+//	for (int k = 0; k <= 2; k++)
+//	{
+//		for (int d = 0; d <= 2; d++)
+//		{
+//			printf("%5d", a[d][k]);
+//		}
+//		printf("\n");
+//	}
+//}
+//#include<stdio.h>
+//
+//void main()
+//{
+//	const int m = 4;
+//	const int n = 3;
+//	long data[m][n];
+//	for (int i = 0; i < m; i++)
+//	{
+//		for (int j = 0; j < n; j++)
+//			data[i][j] = i + j ;
 //	}
 //
-//	hap = aa[0] + aa[1] + aa[2] + aa[3];
+//	for (int i = 0; i < m; i++)
+//	{
+//		for (int j = 0; j < n; j++){
+//			printf( "%3d", data[i][j]);
+//		}
+//		printf("\n");
+//	}
+//}
+//#include <stdio.h>
 //
-//	printf("합계 ==> %d \n", hap);
+//void main()
+//{
+//	const int N = 5;
+//	long data[N][N];
+//	int num = 1;
+//	for (int k = 0; k < N; k++)
+//	{
+//		for (int c = 0; c < N; c++)
+//			data[k][c] = num++;
+//	}
+//	
+//	for (int k = 0; k < N; k++)
+//	{
+//		for (int c = 0; c <N; c++)
+//		{
+//			printf("%3d", data[k][c]);
+//		}
+//		printf("\n");
+//	}
 //}
 //
+//
+//#include <stdio.h>
+//
+//void main()
+//{
+//	const int N = 5;
+//	long data[N][N];
+//	int num = 1;
+//	for (int k = 0; k < N; k++)
+//	{
+//		for (int c = 0; c < N; c++)
+//			data[k][c] = num++;
+//	}
+//
+//	for (int k = N-1; k >= 0; k--)
+//	{
+//		for (int c = N-1; c >= 0; c--)
+//		{
+//			printf("%3d", data[k][c]);
+//		}
+//		printf("\n");
+//	}
+//}
+//
+//#include <stdio.h>
+//
+//void main()
+//{
+//	const int N = 5;
+//	long data1[N*N];
+//	long data[N][N];
+//	int num = 1;
+//	int num1 = 0;
+//	for (int k = 0; k < N; k++)
+//	{
+//		for (int c = 0; c < N; c++)
+//		{
+//			data[k][c] = num++;
+//
+//		}
+//	}
+//	for (int k = 0; k < N; k++)
+//	{
+//		for (int c = 0; c < N; c++)
+//		{
+//			data1[num1] = data[k][c];
+//			num1++;
+//		}
+//	}
+//	for (int k = 0; k < N*N; k++)
+//	{
+//		printf("%d\n", data1[k]);
+//	}
+//}
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int n;
+//	scanf("%d", &n);
+//	int a[10] = {};
+//	char b[10][10] = {};
+//	for (int k = 0; k < n; k++)
+//	{
+//		scanf("%d", &a[k]);
+//	}
+//	for (int c = 0; c <= n; c++)
+//	{
+//		for (int d = 0; d < a[c]; d++)
+//		{
+//			b[d][c] = '*';
+//		}
+//	}
+//	for (int c = 0; c < 10; c++)
+//	{
+//		for (int d = 0; d < 10; d++)
+//		{
+//			if(b[c][d] == 0)printf(" ");
+//			printf("%c", b[c][d]);
+//		}
+//		printf("\n");
+//	}
+//}
+
 #include <stdio.h>
 
-void main()
+int main()
 {
-	int aa[4] = { 100, 200, 300, 400 };
-	int bb[] = { 100, 200, 300, 400 };
-	int dcc[4] = { 100, 200 };
-	int dd[4] = { 0 };
-	int i;
-	for (i = 0; i <= 3; i++)
-		printf("bb[%d]==>%d\t", i, aa[i]);
-	printf("\n");
+	int N = 5;
+	long data[5][5];
+	int num = 1;
+	for (int k = 0; k < 5; k++)
+	{
+		data[0][k] = num++;
+	}
+	for (int k = 0; k < 5; k++)
+	{
+
+	}
 }
