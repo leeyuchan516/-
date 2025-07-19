9d1f1331e82c40cb646924ab2a408e1b39159bc0
leeyuchan516 <yuchan20100516@gmail.com>
2025-07-19 18:00:47 +0900

    Add files via upload

--- 소스.cpp
+++ 소스.cpp
@@ -1,211 +1,346 @@
 //#include <stdio.h>
 //
-//int main()
+//void main()
 //{
-//	int a[10] = { 1, 1, 4, 5, 6, 2, 3, 3, 4, 1 };	
-//	int dice[7] = { };
+//	int aa[3][4];
 //
-//	for (int k = 0; k < 10; k++)//10
-//	{
-//		dice[a[k]]++;
-//	}
-//	for (int k = 1; k <= 6; k++)
-//	{
-//		printf("주사위 %d가 나온 횟수는 %d\n", k, dice[k]);
-//	}
+//	aa[0][0] = 1; aa[0][1] = 2; aa[0][2] = 3; aa[0][3] = 4;
+//	aa[1][0] = 5; aa[1][1] = 6; aa[1][2] = 7; aa[1][3] = 8;
+//	aa[2][0] = 9; aa[2][1] = 10; aa[2][2] = 11; aa[2][3] = 12;
+//
+//	printf("aa[0][0]부터 aa[2][3]까지 출력\n");
+//
+//	printf("%3d %3d %3d %3d\n", aa[0][0], aa[0][1], aa[0][2], aa[0][3]);
+//	printf("%3d %3d %3d %3d\n", aa[1][0], aa[1][1], aa[1][2]);
+//	printf("%3d %3d %3d %3d\n", aa[2][0], aa[2][1], aa[2][2]);
 //}
-//66
-//16
 //#include <stdio.h>
 //
-//int main()
+//void main()
 //{
-//	char arr[2][3] = { '1', '2', '3', '4', '5', '6' };
-//	printf("%d\n", sizeof(arr));
-//	return 0;
+//	int aa[3][4];
+//	int i, k;
+//
+//	int val = 1;
+//
+//	for (i = 0; i < 3; i++)
+//	{
+//		for (k = 0; k < 4; k++)
+//		{
+//			aa[i][k] = val;
+//			val++;
+//		}
+//	}
+//
+//	printf("aa[0][0]부터 aa[2][3]까지 출력 \n");
+//
+//	for (i = 0; i < 3; i++)
+//	{
+//		for (k = 0; k < 4; k++)
+//		{
+//			printf("%3d", aa[i][k]);
+//		}
+//		printf("\n");
+//	}
+//}
+//#include <stdio.h>
+//
+//void main()
+//{
+//	int gugu[9][9];
+//	int i, k;
+//
+//	for (i = 0; i < 9; i++)
+//		for (k = 0; k < 9; k++)
+//			gugu[i][k] = (i + 1) * (k + 1);
+//
+//	for (i = 0; i < 9; i++)
+//	{
+//		for (k = 0; k < 9; k++)
+//		{
+//			printf("%d*%d=%2d ", k + 1, i + 1, gugu[i][k]);
+//		}
+//		printf("\n");
+//	}
+//}
+//#include <stdio.h>
+//
+//void main()
+//{
+//	const int N = 10;
+//	long data[N][N] = {};
+//	int v = 1;
+//	int y = 0;
+//	int x = -1;
+//	int v1 = N;
+//	int v2 = 1;
+//	for(;;)
+//	{
+//		for (int k = 0; k < v1; k++)
+//		{
+//			x += v2;
+//			data[y][x] = v;
+//			v++;
+//		}
+//		v1--;
+//		if (v1 == 0)
+//		{
+//			break;
+//		}
+//		for (int k = 0; k < v1; k++)
+//		{
+//			y += v2;
+//			data[y][x] = v;
+//			v++;
+//
+//		}
+//		v2 = -v2;
+//		
+//	}
+//
+//	for (int i = 0; i < N; i++)
+//	{
+//		for (int k = 0; k < N; k++)
+//		{
+//			printf("%3d ", data[i][k]);
+//		}
+//		printf("\n");
+//	}
+//}
+
+//#include <stdio.h>
+//
+//void main()
+//{
+//	int b = 100;
+//	int *pB = &b;
+//
+//	printf("b = %d\n", b);
+//	printf("b = %x\n", &b);
+//	printf("b = %d\n", *pB);
+//	printf("b = %x\n", pB);
+//}
+//
+//#include <stdio.h>
+//
+//void main()
+//{
+//	char a = 'A';
+//	char *pA = &a;
+//
+//	int b = 100;
+//	int *pB = &b;
+//
+//	double c = 3.14;
+//	double *pC = &c;
+//
+//	printf("pA의 크기 : %d byte\n", sizeof(pA));
+//	printf("pB의 크기 : %d byte\n", sizeof(pB));
+//	printf("pC의 크기 : %d byte\n", sizeof(pC));
+//
+//	printf("*pA의 크기 : %d byte\n", sizeof(*pA));
+//	printf("*pB의 크기 : %d byte\n", sizeof(*pB));
+//	printf("*pC의 크기 : %d byte\n", sizeof(*pC));
 //}
 
 //#include <stdio.h>
 //
 //int main()
 //{
-//	int arr[2][3] = { {'1', '2', '3'},{'4', '5', '6'} };
-//	printf("%d\n", sizeof(arr));
-//	return 0;
+//	printf("Hello World!");
 //}
 
-//#include <stdio.h>
-//void main()
-//{
-//	int arr2d[5][4] =
-//	{ {662, 7, 4, 74}, {8, 396, 299, 95}, {66, 73, 86, 0}, {84, 7, 41, 11}, };
-//	for (int i = 0; i < 5; i++)
-//	{
-//		for (int j = 0; j < 4; j++)
-//		{
-//			printf("%3d", arr2d[i][j]);
-//		}
-//		printf("\n");
-//	}
-//}
-//
 //#include <stdio.h>
 //
 //int main()
 //{
-//	int a[3][3] = { {2, 4, 6}, {8, 10, 12}, {14, 16, 18} };
-//
-//	for (int k = 0; k <= 2; k++)
-//	{
-//		for (int d = 0; d <= 2; d++)
-//		{
-//			printf("%5d", a[d][k]);
-//		}
-//		printf("\n");
-//	}
+//	int A;
+//	int B;
+//	scanf("%d", &A);
+//	scanf("%d", &B);
+//	printf("%d", A + B);
 //}
-//#include<stdio.h>
-//
-//void main()
-//{
-//	const int m = 4;
-//	const int n = 3;
-//	long data[m][n];
-//	for (int i = 0; i < m; i++)
-//	{
-//		for (int j = 0; j < n; j++)
-//			data[i][j] = i + j ;
-//	}
-//
-//	for (int i = 0; i < m; i++)
-//	{
-//		for (int j = 0; j < n; j++){
-//			printf( "%3d", data[i][j]);
-//		}
-//		printf("\n");
-//	}
-//}
-//#include <stdio.h>
-//
-//void main()
-//{
-//	const int N = 5;
-//	long data[N][N];
-//	int num = 1;
-//	for (int k = 0; k < N; k++)
-//	{
-//		for (int c = 0; c < N; c++)
-//			data[k][c] = num++;
-//	}
-//	
-//	for (int k = 0; k < N; k++)
-//	{
-//		for (int c = 0; c <N; c++)
-//		{
-//			printf("%3d", data[k][c]);
-//		}
-//		printf("\n");
-//	}
-//}
-//
-//
-//#include <stdio.h>
-//
-//void main()
-//{
-//	const int N = 5;
-//	long data[N][N];
-//	int num = 1;
-//	for (int k = 0; k < N; k++)
-//	{
-//		for (int c = 0; c < N; c++)
-//			data[k][c] = num++;
-//	}
-//
-//	for (int k = N-1; k >= 0; k--)
-//	{
-//		for (int c = N-1; c >= 0; c--)
-//		{
-//			printf("%3d", data[k][c]);
-//		}
-//		printf("\n");
-//	}
-//}
-//
-//#include <stdio.h>
-//
-//void main()
-//{
-//	const int N = 5;
-//	long data1[N*N];
-//	long data[N][N];
-//	int num = 1;
-//	int num1 = 0;
-//	for (int k = 0; k < N; k++)
-//	{
-//		for (int c = 0; c < N; c++)
-//		{
-//			data[k][c] = num++;
-//
-//		}
-//	}
-//	for (int k = 0; k < N; k++)
-//	{
-//		for (int c = 0; c < N; c++)
-//		{
-//			data1[num1] = data[k][c];
-//			num1++;
-//		}
-//	}
-//	for (int k = 0; k < N*N; k++)
-//	{
-//		printf("%d\n", data1[k]);
-//	}
-//}
-//
+
 //#include <stdio.h>
 //
 //int main()
 //{
-//	int n;
-//	scanf("%d", &n);
-//	int a[10] = {};
-//	char b[10][10] = {};
-//	for (int k = 0; k < n; k++)
-//	{
-//		scanf("%d", &a[k]);
-//	}
-//	for (int c = 0; c <= n; c++)
-//	{
-//		for (int d = 0; d < a[c]; d++)
-//		{
-//			b[d][c] = '*';
-//		}
-//	}
-//	for (int c = 0; c < 10; c++)
-//	{
-//		for (int d = 0; d < 10; d++)
-//		{
-//			if(b[c][d] == 0)printf(" ");
-//			printf("%c", b[c][d]);
-//		}
-//		printf("\n");
-//	}
+//	int A;
+//	int B;
+//	scanf("%d", &A);
+//	scanf("%d", &B);
+//	printf("%d", A - B);
 //}
 
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int A;
+//	int B;
+//	scanf("%d", &A);
+//	scanf("%d", &B);
+//	printf("%d", A * B);
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	double A;
+//	double B;
+//	scanf("%lf", &A);
+//	scanf("%lf", &B);
+//	printf("%0.16lf", A / B);
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int A;
+//	int B;
+//	scanf("%d", &A);
+//	scanf("%d", &B);
+//	printf("%d\n", A + B);
+//	printf("%d\n", A - B);
+//	printf("%d\n", A * B);
+//	printf("%d\n", A / B);
+//	printf("%d", A % B);
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int year;
+//
+//	scanf("%d", &year);
+//	printf("%d", year - 543);
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int A, B, C;
+//	scanf("%d %d %d", &A, &B, &C);
+//	printf("%d\n", (A + B) % C);
+//	printf("%d\n", ((A % C) + (B % C)) % C);
+//	printf("%d\n", (A*B) % C);
+//	printf("%d", ((A % C) * (B % C)) % C);
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a;
+//	int b;
+//
+//	scanf("%d %d", &a, &b);
+//	printf("%d\n" , a * (b%10));
+//	printf("%d\n", a * ( (b / 10) - (b / 100 * 10)));
+//	printf("%d\n", a * (b / 100));
+//	printf("%d", a * b);
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	long long int A, B, C;
+//	scanf("%lld", &A);
+//	scanf("%lld", &B);
+//	scanf("%lld", &C);
+//	printf("%lld", A + B + C);
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("\\    /\\\n");
+//	printf(" )  ( ')\n");
+//	printf("(  /  )\n");
+//	printf(" \\(__)|\n");
+//}
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	printf("|\\_/|\n");
+//	printf("|q p|   /}\n");
+//	printf("( 0 )\"\"\"\\\n");
+//	printf("|\"^\"`    |\n");
+//	printf("||_/=\\\\__|");
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int A;
+//	int B;
+//	scanf("%d %d", &A, &B);
+//	if (A > B)
+//	{
+//		printf(">");
+//	}
+//	if (A < B)
+//	{
+//		printf("<");
+//	}
+//	if (A == B)
+//	{
+//		printf("==");
+//	}
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int A;
+//	scanf("%d", &A);
+//
+//	if (90 <= A)
+//		printf("A");
+//	else if (80 <= A)
+//		printf("B");
+//	else if (70 <= A)
+//		printf("C");
+//	else if (60 <= A)
+//		printf("D");
+//	else
+//		printf("F");
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a;
+//	scanf("%d", &a);
+//
+//	if (a % 4 == 0 && a % 100 != 0 || a % 400 == 0)
+//		printf("1");
+//	else
+//		printf("0");
+//}
 #include <stdio.h>
 
 int main()
 {
-	int N = 5;
-	long data[5][5];
-	int num = 1;
-	for (int k = 0; k < 5; k++)
+	int x, y;
+	scanf("%d %d", &x, &y);
+	if (x > 0)
 	{
-		data[0][k] = num++;
+		if (y > 0)
+			printf("1");
+		if (y < 0)
+			printf("4");
 	}
-	for (int k = 0; k < 5; k++)
+	if (x < 0)
 	{
-
+		if(y>)
 	}
 }
