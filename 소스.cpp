f26bc68bd7f0bfbb48876ea9a08b9cc6310833d0
leeyuchan516 <yuchan20100516@gmail.com>
2025-08-23 16:12:11 +0900

    Add files via upload

--- 소스.cpp
+++ 소스.cpp
@@ -1,278 +1,64 @@
 //#include <stdio.h>
 //
-//int main()
+//void main()
 //{
-//	int ary[4];
-//	int hap = 0;
-//	int i;
-//	for (i = 0; i <= 3; i++) {
-//		printf("%d번쨰 숫자를 입력하세요 : ", i + 1);
-//		scanf("%d", &ary[i]);
-//	}
-//	for (i = 3; i > 0; i--)
-//		hap += ary[i];
-//	printf("%d", hap);
+//	int b = 100;
+//	int* pB = &b;
+//
+//	printf("b = %d\n", b);
+//	printf("b = %x\n", &b);
+//	printf("b = %d\n", *pB);
+//	printf("b = %x\n", pB);
 //}
-
 //#include <stdio.h>
 //
-//int main()
-//{
-//	int ary[2][3];
-//	int i, k;
-//	int num = 10;
-//	for (i = 0; i < 2; i++)
-//		for (k = 0; k < 3; k++)
-//			ary[i][k] = --num;
+//void main() {
+//	char a = 'A';
+//	char* pA = &a;
 //
-//	for (i = 0; i < 2; i++) {
-//		for (k = 0; k < 3; k++) {
-//			printf("%3d", ary[i][k]);
-//		}
-//		printf("\n");
-//	}
+//	int b = 100;
+//	int* pB = &b;
+//
+//	double c = 3.14;
+//	double* pC = &c;
+//
+//	printf("pA의 크기 : %d byte\n", sizeof(pA));
+//	printf("pB의 크기 : %d byte\n", sizeof(pB));
+//	printf("pC의 크기 : %d byte\n", sizeof(pC));
+//
+//	printf("*pA의 크기 : %d byte\n", sizeof(*pA));
+//	printf("*pB의 크기 : %d byte\n", sizeof(*pB));
+//	printf("*pC의 크기 : %d byte\n", sizeof(*pC));
 //}
-
 //#include <stdio.h>
 //
-//int main()
+//void main()
 //{
-//	int a, b;
-//	scanf("%d %d", &a, &b);
-//	if (a > 0)
-//	{
-//		if (b > 0)
-//		{
-//			printf("1");
-//		}
-//		if (b < 0)
-//		{
-//			printf("4");
-//		}
-//	}
-//	if (a < 0)
-//	{
-//		if (b > 0)
-//		{
-//			printf("2");
-//		}
-//		if (b < 0)
-//		{
-//			printf("3");
-//		}
-//	}
+//	char str[] = "programming";
+//	char* ptr1, * ptr2;
+//	ptr1 = &str[0];
+//	ptr2 = &str[7];
+//
+//	printf("두 포인터 간의 차는 %d입니다\n", ptr2 - ptr1);
+//
+//	printf("%x\n", ptr1 + 1);
+//	printf("%c\n", *(ptr1 + 1));
+//
+//	printf("%x\n", ptr1 + 3);
+//	printf("%c\n", *(ptr1 + 3));
 //}
+#include <stdio.h>
 
-//#include <stdio.h>
-//
-//int main()
-//{
-//	int H , M;
-//	scanf("%d %d", &H, &M);
-//
-//	M += 15;
-//	H -= 1;
-//
-//	H += M / 60;
-//	M %= 60;
-//
-//	H += 24;
-//	H %= 24;
-//
-//
-//	/*if (M < 45)
-//	{
-//		M = M + 15;
-//		if (H == 0)
-//		{
-//			H = 24;
-//		}
-//		H = H - 1;
-//	}
-//	else if (M >= 45)
-//	{
-//		M = M - 45;
-//	}*/
-//
-//	printf("%d %d", H, M);
-//}
+void main()
+{
+	int* numptr;
+	int num1 = 10;
+	int num2 = 20;
 
-//#include <stdio.h>
-//
-//int main()
-//{
-//	int H, M, Cooking_time;
-//	scanf("%d %d", &H, &M);
-//	scanf("%d", &Cooking_time);
-//
-//	M += Cooking_time;
-//	H += M / 60;
-//	M %= 60;
-//	H += 24;
-//	H %= 24;
-//
-//
-//	printf("%d %d", H, M);
-//}
+	numptr = &num1;
+	printf("%d\n", *numptr);
 
-//#include <stdio.h>
-//
-//int main()
-//{
-//	int dice1, dice2, dice3;
-//
-//	scanf("%d %d %d", &dice1, &dice2, &dice3);
-//
-//	if (dice1 == dice2 && dice1 == dice3)
-//	{
-//		printf("%d", 10000 + (dice1 * 1000));
-//	}
-//	else if (dice1 == dice2)
-//	{
-//		printf("%d", 1000 + dice1 * 100);
-//	}
-//	else if (dice1 == dice3)
-//	{
-//		printf("%d", 1000 + dice1 * 100);
-//	}
-//	else if (dice2 == dice3)
-//	{
-//		printf("%d", 1000 + dice2 * 100);
-//	}
-//	else
-//	{
-//		if (dice1 > dice2)
-//		{
-//			if (dice1 > dice3)
-//			{
-//				printf("%d", dice1 * 100);
-//			}
-//			else
-//			{
-//				printf("%d", dice3 * 100);
-//			}
-//		}
-//		else
-//		{
-//			if (dice2 > dice3)
-//			{
-//				printf("%d", dice2 * 100);
-//			}
-//			else
-//			{
-//				printf("%d", dice3 * 100);
-//			}
-//		}
-//	}
-//	return 0;
-//}
-
-
-//	if (dice1 == dice2)
-//	{
-//		if (dice1 == dice3)
-//		{
-//			printf("%d", 10000 + (dice1 * 1000));
-//		}
-//		if (dice1 != dice3)
-//		{
-//			printf("%d", 1000 + dice1 * 100);
-//		}
-//	}
-//	else if(dice1 != dice2)
-//	{
-//		if (dice1 != dice3)
-//		{
-//			if (dice1 > dice2)
-//			{
-//				if (dice1 > dice3)
-//				{
-//					printf("%d", dice1 * 100);
-//				}
-//				if (dice1 < dice2)
-//				{
-//					printf("%d", dice2 * 100);
-//				}
-//			}
-//			if (dice1 < dice2)
-//			{
-//				if (dice2 > dice3)
-//				{
-//					printf("%d", dice2 * 100);
-//				}
-//				if (dice2 < dice3)
-//				{
-//					printf("%d", dice3 * 100);
-//				}
-//			}
-//		}
-//		if (dice1 == dice3)
-//		{
-//			printf("%d", 1000 + dice1 * 100);
-//		}
-//		if (dice2 == dice3)
-//		{
-//			printf("%d", 1000 + dice2 * 100);
-//		}
-//	}
-
-
-//#include <stdio.h>
-//
-//int main()
-//{
-//	int N;
-//	scanf("%d", &N);
-//	for (int k=1; k<=9; k++)
-//	{
-//		printf("%d * %d = %d\n", N, k, N * k);
-//	}
-//	return 0;
-//}
-
-//#include <stdio.h>
-//int main()
-//{
-//	int A;
-//	int B;
-//	int T;
-//	scanf("%d", &T);
-//	for (;T>0;T--)
-//	{
-//		scanf("%d %d", &A, &B);
-//		printf("%d\n", A + B);
-//	}
-//}
-
-//#include <stdio.h>
-//
-//int main()
-//{
-//	int n;
-//	int sum = 0;
-//	scanf("%d", &n);
-//	for (int k = 0; k <= n; k++)
-//	{
-//		sum += k;
-//	}
-//	printf("%d", sum);
-//}
-
-//#include <stdio.h>
-//
-//int main()
-//{
-//	int X, N, a, b;
-//	int sum = 0;
-//	scanf("%d", &X);
-//	scanf("%d", &N);
-//	for (;N>0;N--)
-//	{
-//		scanf("%d %d", &a, &b);
-//		sum += a * b;
-//	}
-//	if (X == sum)
-//		printf("Yes");
-//	else
-//		printf("No");
-//}
+	numptr = &num2;
+	printf("%d\n", *numptr);
+		
+}
