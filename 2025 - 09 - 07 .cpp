c6562b95c9286d9358cc046ecd7bb1a4ccb8d85f
leeyuchan516 <yuchan20100516@gmail.com>
2025-09-07 13:46:50 +0900

    Add files via upload

---
+++ 2025 - 09 - 07 .cpp
@@ -0,0 +1,65 @@
+//#include <stdio.h>
+//
+//void main()
+//{
+//	char ss[5] = "abcd";
+//	char tt[5];
+//	int i;
+//
+//	for (int i = 0; i < 4; i++)
+//	{
+//		tt[i] = ss[3 - i];
+//	}
+//	tt[4] = '\0';
+//	printf("거꾸로 출력한 결과 ==> %s \n", tt);
+//}
+//#define _CRT_SUCURE_NO_WARNINGS
+//#include <string.h>
+//#include <stdio.h>
+//void main()
+//{
+//	char ss[] = "XYZ";
+//	int len;
+//
+//	len = strlen(ss);
+//
+//	printf("문자열 \"%s\"의 길이 ==> %d \n", ss, len);
+//}
+//
+//#define _CRT_SUCURE_NO_WARNINGS
+//#include <string.h>
+//#include <stdio.h>
+//void main()
+//{
+//	char ss[4];
+//
+//	strcpy(ss, "XYZ");
+//
+//	printf("문자열 ss의 내용 ==> %s \n", ss);
+//}
+//
+//#define _CRT_SUCURE_NO_WARNINGS
+//#include <string.h>
+//#include <stdio.h>
+//void main()
+//{
+//	char ss[7] = "XYZ";
+//
+//	strcat(ss, "ABC");
+//
+//	printf("이어진 문자열 ss의 내용 ==> %s \n", ss);
+//}
+//
+//#define _CRT_SUCURE_NO_WARNINGS
+//#include <string.h>
+//#include <stdio.h>
+//void main()
+//{
+//	char ss[] = "XYZ";
+//	char tt[] = "xyz";
+//	int r;
+//
+//	r = strcmp(ss, tt);
+//
+//	printf("두 문자열의 비교결과 ==> %d \n", r);
+//}
