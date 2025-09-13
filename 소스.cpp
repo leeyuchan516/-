f34238ed8780ca85925a1dc36ad25aba65bc6388
leeyuchan516 <yuchan20100516@gmail.com>
2025-09-13 18:02:26 +0900

    Add files via upload

--- 소스.cpp
+++ 소스.cpp
@@ -1,183 +1,15 @@
 //#include <stdio.h>
-//int main()
-//{
-//	char a[6][16] = {};
-//	char b;
-//
-//	for (int k = 1; k <= 5; k++)
-//	{
-//		for (int i = 1; i <= 16; i++)
-//		{
-//			scanf("%c", &b);
-//			if (b == '\n')
-//				break;
-//			a[k][i] = b;
-//		}
-//	}
-//
-//	for (int i = 1; i <= 15; i++)
-//	{
-//		for (int k = 1; k <= 5; k++)
-//		{
-//			if (a[k][i] != 0)
-//			{
-//				printf("%c", a[k][i]);
-//			}
-//		}
-//	}
-//	return 0;
-//}
-//
-//#include <stdio.h>
-//
-//int main()
-//{
-//	int input;
-//	int s[101][101] = {};
-//	int x;
-//	int y;
-//	int input2;
-//	int sum = 0;
-//
-//	scanf("%d", &input);
-//
-//
-//	for (int k = 1; k <= input; k++)
-//	{
-//	 	scanf("%d", &x);
-//		scanf("%d", &y);
-//
-//		for (int i = 1; i <= 10; i++)
-//		{
-//			for (int a = 1; a <= 10; a++)
-//			{
-//				s[x + a][y + i] = 1;
-//			}
-//		}
-//	}
-//
-//	for (int k = 1; k <= 100; k++)
-//	{
-//		for (int i = 1; i <= 100; i++)
-//		{
-//			if (s[k][i] == 1)
-//			{
-//				sum++;
-//			}
-//		}
-//	}
-//
-//	printf("%d", sum);
-//	return 0;
-//}
-//#include <stdio.h>
-//
-//int main()
-//{
-//	int A[11];
-//	int sum[42] = {};
-//	int sum1 = 0;
-//
-//	for (int k = 1; k <= 10; k++)
-//	{
-//		scanf("%d", &A[k]);
-//	}
-//
-//	for (int k = 1; k <= 10; k++)
-//	{
-//		for (int i = 0; i <= 41; i++)
-//		{
-//			if (A[k] % 42 == i)
-//			{
-//				sum[i] = 1;
-//			}
-//		}
-//	}
-//	for (int k = 0; k <= 41; k++)
-//	{
-//		if (sum[k] == 1)
-//		{
-//			sum1++;
-//		}
-//	}
-//	printf("%d", sum1);
-//}
-//#include <stdio.h>
-//#include <string.h>
-//int main()
-//{
-//	char str[] = "memmove can be very useful.......";
-//	memmove(str + 20, str + 15, 11);
-//	puts(str);
-//	return 0;
-//}
-
-//#include <stdio.h>
-//#include <string.h>
-//struct mydata
-//{
-//	int a;
-//	char b[25];
-//};
-//void main()
-//{
-//	char buf[5];
-//	memset(buf, '1', 5);
-//	printf("%s\n", buf);
-//
-//	buf[4] = 0x00;
-//	printf("%s\n", buf);
-//
-//	struct mydata data;
-//	memset((void*)&data, 0x00, sizeof(data));
-//	data.b[0] = 'k';
-//	printf("%d - %s\n", data.a, data.b);
-//}
-//#include <stdio.h>
 //#include <string.h>
 //
 //int main()
 //{
-//	char* str1 = (char*)"coding";
-//	char* str2 = (char*)"coding";
-//	char* str3 = (char*)"coding.ne.kr";
-//	char* str4 = (char*)"ne.kr";
-//	char* str5 = (char*)"abcd";
+//	char str[] = "This is a simple string";
+//	char* pch;
 //
-//	printf("%s with %s = %d\n", str1, str2, memcmp(str1, str2, strlen(str1)));
-//	printf("%s with %s(str1 size) = %d\n", str1, str3, memcmp(str1, str2, strlen(str1)));
-//	printf("%s with %s(str2 size) = %d\n", str1, str3, memcmp(str1, str4, strlen(str1)));
+//	pch = strstr(str, "simple");
+//	strncpy(pch, "simple", 6);
 //
-//	printf("%s with %s = %d\n", str1, str4, memcmp(str1, str4, strlen(str1)));
-//	printf("%s with %s (str1 size) = %d\n", str1, str5, memcmp(str1, str5, strlen(str1)));
-//	printf("%s with %s (str5 size) = %d\n", str1, str5, memcmp(str1, str5, strlen(str1)));
-//	printf("%s with %s (str5 size) = %d\n", str1, str5, memcmp(str1, str5, strlen(str5)));
-//	return 0;
-//}
-//
-//#include <stdio.h>
-//#include <string.h>
-//
-//int main()
-//{
-//	int src_data[10] = { 0, 1, 2, 3, 4, 5 };
-//	int dst_data[10] = { 10, 20, 30, 40, 50 };
-//	memcpy(dst_data, src_data, sizeof(int) * 4);
-//
-//	printf("%d %d %d %d %d\n", src_data[0], src_data[1], src_data[2], src_data[3], src_data[4]);
-//	printf("%d %d %d %d %d\n", dst_data[0], dst_data[1], dst_data[2], dst_data[3], dst_data[4]);
-//}
-//
-//#include <stdio.h>
-//#include <string.h>
-//
-//int main()
-//{
-//	char str[20] = "coding";
-//
-//	strcat(str, ".ne.kr");
 //	printf("%s\n", str);
-//
 //	return 0;
 //}
 //
@@ -186,11 +18,64 @@
 //
 //int main()
 //{
-//	char str[10];
+//	const char* str1 = "LPUX";
+//	const char* str2 = "HINUX";
+//	const char* str3 = "SOLARIS";
 //
-//	scanf("%s", str);
-//	printf("입력한 문자열 : %s\n", str);
-//	printf("문자열의 길이 : %d\n", strlen(str));
+//	printf("s1[%-7s], s2[%-7s] => ret = [%2d]\n", str1, str2, strcmp(str1, str2));
+//	printf("s2[%-7s], s3[%-7s] => ret = [%2d]\n", str2, str3, strcmp(str2, str3));
+//	printf("s3[%-7s], s1[%-7s] => ret = [%2d]\n", str3, str1, strcmp(str3, str1));
+//
+//	return 0;
+//}
+//#include <stdio.h>
+//#include <string.h>
+//
+//int main()
+//{
+//	char buf[25];
+//
+//	strcpy(buf, "hello world");
+//	printf("%s\n", buf);
+//	strncpy(buf, "hello world", 5);
+//	printf("%s\n", buf);
+//
+//#include <stdio.h>
+//#include <string.h>
+//
+//int main()
+//{
+//	char buff[100];
+//	char* output;
+//	char input;
+//	printf(">>");
+//	printf("문장 입력 : ");
+//	gets_s(buff);
+//
+//	for (;;)
+//	{
+//		printf("검색할 문자 입력 : ");
+//		scanf(" %c", &input);
+//		if (input == '0')
+//		{
+//			printf("종료합니다.\n");
+//			break;
+//		}
+//		output = strchr(buff, input);
+//		if(output == NULL)
+//		{
+//			printf("없습니다.\n");
+//		}
+//		else
+//		{
+//			while (output != NULL)
+//			{
+//				printf("%d번째에 있습니다.\n", output - buff + 1);
+//				output = strchr(output + 1, input);
+//			}
+//		}
+//		printf("\n");
+//	}
 //}
 //#include <stdio.h>
 //#include <string.h>
@@ -209,3 +94,105 @@
 //	}
 //	return 0;
 //}
+//#include <stdio.h>
+////#include <string.h>
+//
+//int mystring(char *m)
+//{
+//	int result = 0;
+//	while (*m != NULL)
+//	{
+//		result++;
+//		m++;
+//	}
+//	return result;
+//}
+//
+//
+//int main()
+//{
+//
+//	char buff1[100] = "abfdasc";
+//	int len = mystring(buff1);
+//	printf("%d\n", len);
+//}
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//
+//int parsing(char* string, int* data)
+//{
+//	char* p;
+//	int cnt;
+//	cnt = 0;
+//	p = strtok(string, "_");
+//	//while (p) {
+//	//	data[cnt++] = atoi(p);
+//
+//	//	p = strtok(NULL, "_");
+//	//}
+//	data[cnt++] = atoi(p);
+//
+//	p = strtok(NULL, "_");
+//
+//	data[cnt++] = atoi(p);
+//
+//	p = strtok(NULL, "_");
+//
+//	data[cnt++] = atoi(p);
+//
+//	p = strtok(NULL, "_");
+//
+//	data[cnt++] = atoi(p);
+//
+//	p = strtok(NULL, "_");
+//
+//	data[cnt++] = atoi(p);
+//	
+//	p = strtok(NULL, "_");
+//
+//
+//	return cnt;
+//}
+//int main(){
+//	char String[] = "101_102_103_104_105";
+//	int data[10];
+//	int cnt, i;
+//
+//	cnt = parsing(String, data);
+//
+//	for (i = 0; i < cnt; i++) {
+//		printf("%d\n", data[i]);
+//	}
+//	return 0;
+//}
+//
+#include <stdio.h>
+int main()
+{
+	int integer = 123;
+	char character = 'c';
+	char string[] = "hellow, world";
+	int* pointer = &integer;
+	double pi = 3.141592;
+	char buf[100];
+
+	sprintf(buf, "integer : (decimal) %d (octal) %o \n", integer, integer);
+	printf("%s \n", buf);
+
+	sprintf(buf, "character : %c \n", character);
+	printf("%s \n", buf);
+
+	sprintf(buf, "string : %c \n", string);
+	printf("%s \n", buf);
+
+	sprintf(buf, "pointer addr : %p \n", pointer);
+	printf("%s \n", buf);
+
+	sprintf(buf, "floating point : %e //%f \n", pi, pi);
+	printf("%s \n", buf);
+
+	sprintf(buf, "percnet symbol : %% \n");
+	printf("%s \n", buf);
+	return 0;
+}
